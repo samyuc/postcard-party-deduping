8f685a035f911cac27c45a167f2a8b3280a4deea
samyuc <33384568+samyuc@users.noreply.github.com>
2019-01-01 15:56:26 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
-Query 1: Exact matches for Format B
+-- Query 1: Exact matches for Format B
 SELECT FB.Name1, FB.Address, PM.Name1, PM.City, PM.State, PM.Zip5, PM.Zip4, PM.Address
 FROM [Format B ] AS FB, [Postcard Party Master] AS PM
 WHERE (((FB.Name1)=[PM].[Name1]) AND ((FB.Address)=[PM].[Address]) AND ((FB.Zip5)=[PM].[Zip5]) AND ((FB.City)=[PM].[City]) AND ((FB.State)=[PM].[State]));
 
-Query 2: Exact matches for Format C
+-- Query 2: Exact matches for Format C
 SELECT FC.Name1, FB.Address, PM.Name1, PM.City, PM.State, PM.Zip5, PM.Zip4, PM.Address
 FROM [Format C ] AS FB, [Postcard Party Master] AS PM
 WHERE (((FC.Name1)=[PM].[Name1]) AND ((FC.Address)=[PM].[Address]) AND ((FC.Zip5)=[PM].[Zip5]) AND ((FC.City)=[PM].[City]) AND ((FC.State)=[PM].[State]));
 
 
-Query 3: Non-exact name matches, exact address/zipcode matches for Format B
+-- Query 3: Non-exact name matches, exact address/zipcode matches for Format B
 SELECT FB.ID, FB.Name1, FB.Address,  FB.Zip5, PM1.Name1, PM1.Address, PM1.Zip5, PM1.ID
 FROM [Format B ] AS FB, [Postcard Party Master] AS PM1
 WHERE NOT EXISTS
@@ -27,7 +27,7 @@ FB.City = PM1.City AND
 FB.State = PM1.State
 ORDER BY FB.ID;
 
-Query 4: Non-exact name matches, exact address/zipcode matches for Format C
+-- Query 4: Non-exact name matches, exact address/zipcode matches for Format C
 SELECT FC.ID, FC.Name1, FC.Address,  FC.Zip5, PM1.Name1, PM1.Address, PM1.Zip5, PM1.ID
 FROM [Format C ] AS FC, [Postcard Party Master] AS PM1
 WHERE NOT EXISTS
